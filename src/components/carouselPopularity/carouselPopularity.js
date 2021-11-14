8748027fdfbc48b9f6fa99fff6d7a91e1bc1a61f
HenriqueY-NIXD <henriqueyamada2004@gmail.com>
2021-11-13 21:30:27 -0300

    Tab bar na home

--- src/components/carouselPopularity/carouselPopularity.js
+++ src/components/carouselPopularity/carouselPopularity.js
@@ -1,28 +1,53 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Alert, TouchableHighlight } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Animated, TouchableHighlight } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
+const items = ["Mais curtidas", "Mais recentes", "Em alta"];
+
+const Tab = (props) => {
+    return(
+        <View style={style.itemContainer}>
+            <TouchableHighlight underlayColor="#ddd" activeOpacity={1} onPress={() => alert("lol")} style={style.item} >
+                <Text>{props.name}</Text>
+            </TouchableHighlight>
+        </View>
+        
+    );
+}
+
+const TabComSeparador = i => {
+    return(
+        <>
+            <Tab name={i.props} key={i.props}/>
+            <View style={style.separador}></View>
+        </>
+    );
+}
+
+const TabSemSeparador = i => {
+    return (
+        <Tab name={i.props} key={i.props}/>
+    );
+}
+
 const CarouselPopularity = () => {
+    let contador = 0;
     return (
         <View style={style.container}>
             <ScrollView horizontal>
-                <View style={style.itemContainer}>
-                    <TouchableHighlight underlayColor="#ddd" activeOpacity={1} onPress={() => alert("lol")} style={style.item} >
-                        <Text>Mais curtidas</Text>
-                    </TouchableHighlight>
-                </View>
-                <View style={style.separador}></View>
-                <View style={style.itemContainer}>
-                    <TouchableHighlight underlayColor="#ddd" activeOpacity={1} onPress={() => alert("lol")} style={style.item} >
-                        <Text>Mais recentes</Text>
-                    </TouchableHighlight>
-                </View>
-                <View style={style.separador}></View>
-                <View style={style.itemContainer}>
-                    <TouchableHighlight underlayColor="#ddd" activeOpacity={1} onPress={() => alert("lol")} style={style.item} >
-                        <Text>Em alta</Text>
-                    </TouchableHighlight>
-                </View>
+                {items.map((i) => {
+                    contador++;
+                    if(contador != 3){
+                        return(
+                            <TabComSeparador props={i}/>
+                        );
+                    }
+                    else{
+                        return(
+                            <TabSemSeparador props={i} />
+                        );
+                    }
+                })}
             </ScrollView>
         </View>
     );
